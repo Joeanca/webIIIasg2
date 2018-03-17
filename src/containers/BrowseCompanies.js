30518e8ec627dc08ea407916c2b4d1c39bbb74b7
Jorge <jcast236@mtroyal.ca>
2018-03-17 05:35:58 +0000

    working on browsecompanies

--- src/containers/BrowseCompanies.js
+++ src/containers/BrowseCompanies.js
@@ -3,7 +3,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
-import jsondata from 'jsonFiles/companies.json';
 
 class BrowseCompanies extends Component {
     constructor(props){
@@ -12,15 +11,11 @@ class BrowseCompanies extends Component {
             companies: []
         }
     }
-    
+
     componentDidMount(){
-        fetch(jsondata)
-          .then((res) => res.json())
-          .then((data) => {
-            this.setState({companies:data});
-            console.log('data:', data);
-          })
-        /*axios.get('address').then(response => {
+        var jsondata = require('./jsonFiles/companies.json');
+        console.log(jsondata);
+        /*axios.get(process.env.PUBLIC_URL + ).then(response => {
             this.setState({companies:response.data.sort((a,b)=>{ let result  =0; if(a.name>b.name){result=1;}else if(b.name>a.name){result=-1;} return result;})});
         })
         .catch(function (error){
