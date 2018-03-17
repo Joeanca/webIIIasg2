9ca3df7370eec61f6d3bb02bd7a9d6173388ab44
Jorge <jcast236@mtroyal.ca>
2018-03-17 03:34:39 +0000

    more basic setup

--- src/containers/StockVisualizer.js
+++ src/containers/StockVisualizer.js
@@ -1,7 +1,7 @@
 /* 9.Stock Visualizer. For this view, display a line chart of the close values for a single month for up to three stocks. That is, the x-axis will contain the days, while the y-axis will be money. There should be four drop-down lists: one to select month, the others to select stocks. The drop-down should display symbol and name. Be sure to use different colors for each line. */
 
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 
 class StockVisualizer extends Component {
